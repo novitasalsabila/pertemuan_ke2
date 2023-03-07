e5670b003f4de90e69e3d43e0038e14468bada4c
novitasalsabila <novnovv10@gmail.com>
2023-03-07 11:18:00 +0700

    first commit

---
+++ contoh.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "selamat datang";
+
+    return 0;
+}
---
+++ latihan1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //begin
+    //numeric Luas, p, l
+    double Luas, p, l;
+    //display 'Masukkan panjangnya = '
+    cout << "Masukkan panjangnya = ";
+    //acceppt p
+    cin >> p;
+    //display 'Masukkan luasnya = '
+    cout << "Masukkan lebarnya = ";
+    //accept l
+    cin >> l;
+    //compute Luas = p*l
+    Luas = p*l;
+    //display "Luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    //end
+
+}
